b1586f002d18e3063009aa5bfc7c4c000e8877bb
Dane Schneider <dane@envkey.com>
2023-10-03 11:45:31 -0700

    apicommunity-v2.3.4, apienterprise-v2.3.6

--- public/app/api/runtimes/express/src/routes.ts
+++ public/app/api/runtimes/express/src/routes.ts
@@ -5,13 +5,65 @@ import { errorFallbackMiddleware, getErrorHandler } from "./errors";
 import { extractIpHost, okResult } from "./routes/route_helpers";
 import { bindFetchRoutes } from "./routes/fetch_routes";
 import { log } from "@core/lib/utils/logger";
+import { getNewTransactionConn, getDb } from "../../../shared/src/db";
 
-export const health: express.RequestHandler<{}, Api.Net.OkResult> = (
-  req,
-  res
-) => {
+export const health: express.RequestHandler<
+  {},
+  Api.Net.OkResult | Api.Net.ErrorResult
+> = async (req, res) => {
   res.status(200);
   res.send(okResult);
+};
+
+const DB_TIMEOUT = 3000;
+export const dbHealth: express.RequestHandler<
+  {},
+  Api.Net.OkResult | Api.Net.ErrorResult
+> = async (req, res) => {
+  try {
+    const txn = await getNewTransactionConn();
+    try {
+      await Promise.race([
+        getDb(
+          { pkey: "test", skey: "test" },
+          {
+            transactionConn: txn,
+          }
+        ),
+        new Promise((_, reject) =>
+          setTimeout(
+            () => reject(new Error("Database operation timed out")),
+            DB_TIMEOUT
+          )
+        ),
+      ]);
+
+      res.status(200);
+      res.send(okResult);
+    } catch (err) {
+      if (err.message === "Database operation timed out") {
+        res.status(408); // 408 Request Timeout status code
+      } else {
+        res.status(500);
+      }
+      res.send({
+        type: "error",
+        error: true,
+        errorStatus: res.statusCode,
+        errorReason: "dbError",
+      });
+    } finally {
+      await txn.release();
+    }
+  } catch (err) {
+    res.status(500);
+    res.send({
+      type: "error",
+      error: true,
+      errorStatus: 500,
+      errorReason: "dbError",
+    });
+  }
 };
 
 export const action: express.RequestHandler<
@@ -46,6 +98,7 @@ export default (app: express.Application) => {
   app.disable("x-powered-by");
 
   app.get("/health", health);
+  app.get("/db-health", dbHealth);
   app.get("/", (req, res) => res.end("API OK"));
 
   app.use((req, res, next) => {
